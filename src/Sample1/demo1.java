7fdfb491a89ad4faedbcc27cad0292f74c94d93b
Vaibhavi <Vaibhavi@Vaibhavi>
2022-09-12 16:49:32 +0530

    [Vaibhavi] Created DemoProject

---
+++ src/Sample1/demo1.java
@@ -0,0 +1,8 @@
+package Sample1;
+
+public class demo1 {
+	public static void main(String[] args) {
+		System.out.println("Running demo1 class");
+	}
+
+}
---
+++ src/Sample1/demo2.java
@@ -0,0 +1,8 @@
+package Sample1;
+
+public class demo2 {
+	public static void main(String[] args) {
+		System.out.println("Running Demo2 Class");
+	}
+
+}
